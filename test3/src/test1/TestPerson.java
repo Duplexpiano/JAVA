1cae538156d310ada7f22e9b6794c49e19614f3b
Duplex <55338260+Duplexpiano@users.noreply.github.com>
2020-10-11 17:29:02 +0800

    Add files via upload

---
+++ test3/src/test1/TestPerson.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class TestPerson {
+	// 测试类,对Person进行测试
+	public static void main(String[] args) {
+
+		Person per = new Person("Paul", "F", 19);// 实例化
+		System.out.println(per); // toString
+	}
+
+}
