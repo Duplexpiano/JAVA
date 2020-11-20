d5289449d3b07dea0f0618f9e0d93560846108bf
Duplex <55338260+Duplexpiano@users.noreply.github.com>
2020-11-20 17:29:42 +0800

    Add files via upload

---
+++ three/src/three/BrushTest.java
@@ -0,0 +1,19 @@
+package three;
+
+import java.awt.EventQueue;
+
+public class BrushTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new MyJFrame();
+			}
+		});
+	}
+
+}
---
+++ three/src/three/MyJFrame.java
@@ -0,0 +1,14 @@
+package three;
+
+import javax.swing.JFrame;
+
+public class MyJFrame extends JFrame {
+
+	public MyJFrame() {
+		this.setVisible(true);
+		this.setSize(1200, 800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.add(new BrushComponent());
+	}
+}
